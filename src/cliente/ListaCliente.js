66e9bd85fe39c3ca8e54e8c5362ebd9b564fbbd8
SebitaBravo <sebastianalejandrobravocampos@gmail.com>
2024-08-12 19:31:09 -0400

    return fix

--- src/cliente/ListaCliente.js
+++ src/cliente/ListaCliente.js
@@ -8,19 +8,10 @@ function ListaClientes() {
   useEffect(() => {
     const fetchClientes = async () => {
       try {
-        const response = await axios.get(
-          "/api/cliente?_size=500"
-        );
-        // Verifica que response.data sea un array
-        if (Array.isArray(response.data)) {
-          setCliente(response.data);
-        } else {
-          console.error("Los datos recibidos no son un array:", response.data);
-          setCliente([]); 
-        }
+        const response = await axios.get("/api/cliente?_size=500");
+        setCliente(response.data);
       } catch (error) {
         console.log(error);
-        setCliente([]); 
       }
     };
     fetchClientes();
@@ -49,37 +40,31 @@ function ListaClientes() {
           </tr>
         </thead>
         <tbody>
-          {clientes.length > 0 ? (
-            clientes.map((cliente) => (
-              <tr key={cliente.id_cliente}>
-                <td>{cliente.id_cliente}</td>
-                <td>{cliente.dv}</td>
-                <td>{cliente.nombres}</td>
-                <td>{cliente.apellidos}</td>
-                <td>{cliente.email}</td>
-                <td>{cliente.celular}</td>
-                <td>{cliente.fecha_registro}</td>
-                <td>
-                  <Link
-                    to={`/clientes/eliminar/${cliente.id_cliente}`}
-                    className="btn btn-danger"
-                  >
-                    Eliminar
-                  </Link>
-                  <Link
-                    to={`/clientes/actualizar/${cliente.id_cliente}`}
-                    className="btn btn-warning"
-                  >
-                    Actualizar
-                  </Link>
-                </td>
-              </tr>
-            ))
-          ) : (
-            <tr>
-              <td colSpan="8">No hay clientes disponibles</td>
+          {clientes.map((cliente) => (
+            <tr key={cliente.id_cliente}>
+              <td>{cliente.id_cliente}</td>
+              <td>{cliente.dv}</td>
+              <td>{cliente.nombres}</td>
+              <td>{cliente.apellidos}</td>
+              <td>{cliente.email}</td>
+              <td>{cliente.celular}</td>
+              <td>{cliente.fecha_registro}</td>
+              <td>
+                <Link
+                  to={`/clientes/eliminar/${cliente.id_cliente}`}
+                  className="btn btn-danger"
+                >
+                  Eliminar
+                </Link>
+                <Link
+                  to={`/clientes/actualizar/${cliente.id_cliente}`}
+                  className="btn btn-warning"
+                >
+                  Actualizar
+                </Link>
+              </td>
             </tr>
-          )}
+          ))}
         </tbody>
       </table>
     </div>
